9f17e0a918ee9f630df5fc5fbb17e45c1098f0d2
19Pawel97 <19pawel9772@gmail.com>
2024-06-13 19:43:09 +0200

    Generics ex3

---
+++ src/main/java/generics/Algorithm.java
@@ -0,0 +1,16 @@
+package generics;
+
+public final class Algorithm {
+
+    public static <T> T max(T x, T y) {
+        return x > y ? x : y;
+    }
+
+    //java: bad operand types for binary operator '>'
+    //  first type:  T
+    //  second type: T
+
+    //Operator > nie może być bezpośrednio użyty z generycznymi parametrami typu T, ponieważ generyki w Javie
+    //nie wspierają operatorów porównania (> lub <), chyba że są one ograniczone przez interfejsy takie jak Comparable.
+    //Java nie potrafi wywnioskować, jak porównać dwa generyczne obiekty x i y bez dodatkowych informacji lub ograniczeń na typie T.
+}
